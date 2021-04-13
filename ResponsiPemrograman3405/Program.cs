04de10dcffa6900689284248f41900a08ad0341d
lucasraditya125 <lucasraditya@students.amikom.ac.id>
2021-04-13 15:01:25 +0700

    ResponsiPemrograman3405

---
+++ ResponsiPemrograman3405/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3405
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            datakaryawan karyawan = new datakaryawan();
+            karyawan.nik = "190302123";
+            karyawan.nama = "Paijo";
+            karyawan.gajibulanan = "3,000,000";
+            karyawan.nik = "190302124";
+            karyawan.nama = "Jono";
+            karyawan.gajibulanan = "2,000,000";
+
+            Console.WriteLine("NIK = 190302123 ");
+            Console.WriteLine("Nama = Paijo ");
+            Console.WriteLine("Gaji Bulanan = 3,000,000");
+            Console.WriteLine("NIK = 190302124 ");
+            Console.WriteLine("Nama = Jono ");
+            Console.WriteLine("Gaji Bulanan = 2,000,000");
+        }
+    }
+}
---
+++ ResponsiPemrograman3405/datakaryawan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3405
+{
+    class datakaryawan
+    {
+        public string nik;
+        public string nama;
+        public string gajibulanan;
+
+        public void NIK()
+        {
+          
+        }
+
+    }
+}
